4b9c3ed06edc6019a9ece8f60bd758c8047ea458
15005I <15005@portalcedi.edu.mx>
2025-04-08 08:55:12 -0600

    sign-up

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,20 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CommonModule} from '@angular/common';
+import { FormsModule} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.page.html',
+  styleUrls: ['./sign-up.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class SignupPage implements OnInit {
+export class SignUpPage implements OnInit{
 
-  constructor() { }
+  ngOnInit(){}
 
-  ngOnInit() {
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private alertController: AlertController,
+    private router: Router,
+    private authService: AuthService
+  ){}
+
+  async onSubmit() {
+    try{
+      await this.authService.register(this.email, this.password);
+      const alert = await this.alertController.create({
+        header: 'Signup Success',
+        message: 'You have created successfully!',
+        buttons: ['OK'],
+      });
+      await alert.present();
+      this.router.navigate(['/login']);
+    } catch (error) {
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: 'Please complete all.',
+          buttons: ['OK'],
+        });
+        await alert.present();
+    }
   }
 
+  validateEmail(email: string): boolean {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
+  }
+
+  onSignUp() {
+    this.router.navigateByUrl("login")
+  }
 }
